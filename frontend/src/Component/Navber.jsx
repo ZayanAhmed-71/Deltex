f1897c5606ef14c1d26d63c6fb07350f92a0962f
akon9293 <mohibuddinakon@gmail.com>
2026-08-17 20:03:02 +0600

    navber done

--- frontend/src/Component/Navber.jsx
+++ frontend/src/Component/Navber.jsx
@@ -14,6 +14,7 @@ const Navber = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
+        {/* in this section we link home, all doctor, about as, contract start */}
         <NavLink to='/'>
           <li className='py-1'>Home</li>
           <hr className='border-none outline-none h-0.5 bg-blue-400 w-3/5 m-auto hidden' />
@@ -22,8 +23,7 @@ const Navber = () => {
         <NavLink to='/doctors'>
           <li className='py-1'>All Doctor</li>
           <hr className='border-none outline-none h-0.5 bg-blue-400 w-3/5 m-auto hidden' />
-        </NavLink>
-
+        </NavLink> 
         <NavLink to='/about'>
           <li className='py-1'>About us</li>
           <hr className='border-none outline-none h-0.5 bg-blue-400 w-3/5 m-auto hidden' />
@@ -33,16 +33,25 @@ const Navber = () => {
           <li className='py-1'>Contract</li>
           <hr className='border-none outline-none h-0.5 bg-blue-400 w-3/5 m-auto hidden' />
         </NavLink>
+        {/* in this section we link home, all doctor, about as, contract pages end */}
+
       </ul>
 
       <div>
         {
           token ?
-          <div>
-            <img src={assets.profile_pic} alt="" />
-            <img src={assets.dropdown_icon} alt="" />
-          </div> :
-            <button onClick={() => navigate('/login')} className='bg-blue-400 text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            <div className='flex items-center gap-2 cursor-pointer group relative'>
+              <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+              <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                  <p onClick={()=>navigate('my-appointemts')} className='hover:text-black cursor-pointer'>My Appointemts</p>
+                  <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                </div>
+              </div>
+            </div> :
+            <button onClick={() => navigate('/login')} className='bg-blue-400 text-white px-8 py-2 rounded-full font-light hidden md:block'>Create Account</button>
         }
       </div>
     </div>
